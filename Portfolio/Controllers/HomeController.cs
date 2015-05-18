676b9488e67d21b1b634b80ab9d22d1359826e92
Keith Miller <keithalexmiller@gmail.com>
2015-05-18 13:14:58 -0600

    WebPortfolioV1.0
    First take on web portfolio, contacts page needs to be fixed

---
+++ Portfolio/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //ViewBag.Message = "Keith Miller";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "The Journey";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "I'd love to hear from you!";
+
+            return View();
+        }
+        public ActionResult Work()
+        {
+            ViewBag.Message = "My Resumé";
+
+            return View();
+        }
+    }
+}
